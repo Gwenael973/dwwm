c591f6095f25c8609d491d7f52ae47e4b35dabcb
27010-34-04 <27010-34-04@exchange.ad.afpanet>
2022-05-23 16:46:57 +0200

    update

--- Site web/Ecf2/BD/js/modeleBD.js
+++ Site web/Ecf2/BD/js/modeleBD.js
@@ -164,33 +164,213 @@ jQuery(document).ready(function($) {
 
 
 //////////////////////////////////////////////////////////////////FIN CODE GUILLAUME////////////////////////////////////////////////////////////////////////////////////////////////
+let element = document.getElementById("card");
+// METEO//
+function buttonClickGET() {
+
+    var queryLoc = document.getElementById("queryLoc").value;
 
 
-function serie() {
+
+
+    var url = "https://api.openweathermap.org/data/2.5/weather?q= " + queryLoc + " &appid=c21a75b667d6f7abb81f118dcf8d4611&units=metric"
+        // 0f789aa757a1d5bda2f01f363ab9453c
+
+
+
+    $.get(url, callBackGetSuccess).done(function() {
+            //alert( "second success" );
+        })
+        .fail(function() {
+            alert("error");
+        })
+        .always(function() {
+            //alert( "finished" );
+        });
+}
+var callBackGetSuccess = function(data) {
+        var element = document.getElementById("zone_meteo");
+        element.innerHTML = "La temperature est de " + data.main.temp + "°C";
+    }
+    //METEO//
+
+
+
+
+
+
+
+// Dans un premier temps on va aller recupérer l'id de l'auteur selon la saisie utilisateur (qui sera un input)
+function recherche() {
+    var value = document.querySelector("#search")
+
+
+
+
+    var idAuteurToSave = 0;
+    for (var [idAuteur, auteur] of auteurs.entries()) {
+        if (auteur.nom == value.value) { //remplacer le nom de l'auteur ici par le choix de l'utilisateur
+            //on est sur le bon: on sauvegarde l'id, puis on sort de la boucle
+            console.log("on est làààààààààà  " + idAuteur)
+            idAuteurToSave = parseInt(idAuteur);
+            break;
+        }
+    }
+    // on a notre idAuteur, on fait notre petit filtre
+    if (idAuteurToSave > 0) {
+        for (var [idAlbum, album] of albums.entries()) {
+            if (album.idAuteur == idAuteurToSave) {
+                serie = series.get(album.idSerie);
+                auteur = auteurs.get(album.idAuteur);
+                console.log(album.titre + " N°" + album.numero + " Série:" + serie.nom + " Auteur:" + auteur.nom);
+            }
+        }
+    }
+
+
+
+
+
+}
+
+
+
+
+
+function RechercheParSerie() {
     const srcImg = "images/"; // emplacement des images de l'appli
     const albumDefaultMini = srcImg + "noComicsMini.jpeg";
     const albumDefault = srcImg + "noComics.jpeg";
     const srcAlbumMini = "albumsMini/"; // emplacement des images des albums en petit
-    const srcAlbum = "albums/"; // emplacement des images des albums en grand
 
 
 
 
 
-    console.log("Liste des albums par série");
+    // var element = document.getElementById("card");
 
-    var element = document.getElementById("card");
+    element.innerHTML = " ";
 
 
     for (var [idSerie, serie] of series.entries()) {
         // Recherche des albums de la série
         for (var [idAlbum, album] of albums.entries()) {
             if (album.idSerie == idSerie) {
-                var element2 = document.createElement("p");
-                element2.innerHTML = serie.nom + ", Album N°" + album.numero + " " + album.titre + ", Auteur:" + auteurs.get(album.idAuteur).nom;
+
+                var element2 = document.createElement("div");
+                element2.setAttribute("id", "card2")
+
+                element2.innerHTML = "<div id:'texte'>" +
+                    "<h1>" + serie.nom + "</h1>" +
+                    " "
+
+                +
+                "<h5>" + " N°" + album.numero + "</h5>"
+
+                +
+                " "
+
+                +
+                "<h2>" + album.titre + "</h2>"
+
+                +
+                " "
+
+                +
+                "<h3>" + " Auteur:" + auteurs.get(album.idAuteur).nom + "</h3>"
+
+                +
+                " "
+
+                +
+                "<h4>" + "Prix :" + album.prix + "</h4>"
+
+                +
+                "</div>";
+
 
                 var element3 = document.createElement("div");
+                element3.setAttribute("id", "img")
                 var nomFic = serie.nom + "-" + album.numero + "-" + album.titre;
+
+                // Utilisation d'une expression régulière pour supprimer 
+                // les caractères non autorisés dans les noms de fichiers : '!?.":$
+                nomFic = nomFic.replace(/'|!|\?|\.|"|:|\$/g, "");
+
+
+
+                element3.innerHTML = '<img src="' + srcAlbumMini + nomFic + ".jpg" + '"/>';
+
+            }
+
+        }
+        element.appendChild(element2);
+        element2.appendChild(element3);
+    }
+    console.log("hello");
+
+}
+
+function RechercheParAuteur() {
+
+    element.innerHTML = " ";
+
+
+    var element2 = document.createElement("div");
+    element2.setAttribute("id", "card2")
+
+
+
+    const srcImg = "images/"; // emplacement des images de l'appli
+    const albumDefaultMini = srcImg + "noComicsMini.jpeg";
+    const albumDefault = srcImg + "noComics.jpeg";
+    const srcAlbumMini = "albumsMini/"; // emplacement des images des albums en petit
+    const srcAlbum = "albums/"; // emplacement des images des albums en grand
+
+    console.log("Liste des albums par auteur");
+    for (var [idAuteur, auteur] of auteurs.entries()) {
+        // Recherche des albums de l'auteur
+        for (var [idAlbum, album] of albums.entries()) {
+            if (album.idAuteur == idAuteur) {
+
+                var element2 = document.createElement("div");
+                element2.setAttribute("id", "card2")
+                console.log(auteur.nom + ", Album N°" + album.numero + " " + album.titre + ", Série:" + series.get(album.idSerie).nom);
+
+                element2.innerHTML = "<div id:'texte'>" +
+                    "<h1>" + auteur.nom + "</h1>" +
+                    " "
+
+                +
+                "<h5>" + " N°" + album.numero + "</h5>"
+
+                +
+                " "
+
+                +
+                "<h2>" + " Série:" + series.get(album.idSerie).nom + "</h2>"
+
+                +
+                " "
+
+                +
+                "<h3>" + album.titre + "</h3>"
+
+
+                +
+                " "
+
+                +
+                "<h4>" + "Prix :" + album.prix + "</h4>"
+
+                +
+                "</div>";
+
+
+                var element3 = document.createElement("div");
+                element3.setAttribute("id", "img")
+                var nomFic = series.get(album.idSerie).nom + "-" + album.numero + "-" + album.titre;
+
 
                 // Utilisation d'une expression régulière pour supprimer 
                 // les caractères non autorisés dans les noms de fichiers : '!?.":$
@@ -203,21 +383,25 @@ function serie() {
 
 
 
+
             }
 
         }
         element.appendChild(element2);
         element2.appendChild(element3);
-    }
 
+
+
+    }
 }
 
 
 
 
 
-
 function tousLesAlbums() {
+
+
     const srcImg = "images/"; // emplacement des images de l'appli
     const albumDefaultMini = srcImg + "noComicsMini.jpeg";
     const albumDefault = srcImg + "noComics.jpeg";
@@ -227,19 +411,15 @@ function tousLesAlbums() {
 
 
 
+    // var element = document.getElementById("card");
 
 
-
-
-
-    console.log("Liste des albums");
-
-    var element = document.getElementById("card");
+    element.innerHTML = " ";
 
     albums.forEach(album => {
         serie = series.get(album.idSerie);
         auteur = auteurs.get(album.idAuteur);
-        console.log(album.titre + " N°" + album.numero + " Série:" + serie.nom + " Auteur:" + auteur.nom + album.prix);
+
 
         var element2 = document.createElement("div");
         element2.setAttribute("id", "card2")
@@ -292,6 +472,8 @@ function tousLesAlbums() {
         element.appendChild(element2);
         element2.appendChild(element3);
 
+
+
     });
 
 
@@ -301,33 +483,28 @@ function tousLesAlbums() {
 
 }
 
+// Get the modal
+var modal = document.getElementById("myModal");
 
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
 
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
 
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
 
-
-
-
-
-// for (var [idAuteur, auteur] of auteurs.entries()) {
-//     // Recherche des albums de l'auteur
-//     for (var [idAlbum, album] of albums.entries()) {
-//         if (album.idAuteur == idAuteur) {
-//             console.log(auteur.nom + ", Album N°" + album.numero + " " + album.titre + ", Série:" + series.get(album.idSerie).nom);
-//             card.innerHTML = auteur.nom + ", Album N°" + album.numero + " " + album.titre + ", Série:" + series.get(album.idSerie).nom;
-
-//             document.body.append(card);
-
-
-
-
-
-
-//         }
-//     }
-
-
-// }
-
-// element2.innerHTML = '<div class = "container-image">' + '<img src = "' + srcAlbum + nomFic + ".jpg" + '"/>' + "</div>"
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
